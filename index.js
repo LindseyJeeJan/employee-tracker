a44c2d18ebbfeaaf90cbd3db8d13b65df640ab32
Lindsey JJ <ljjlauria@mac.com>
2021-06-06 06:32:15 -0700

    separated out the connection function

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-require('dotenv').config();
-
-// create the connection information for the sql database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const connection = require('./config/connection');
 
 // connect and give welcome message
 connection.connect((err) => {
